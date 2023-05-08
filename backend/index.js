288aea753d6807cf9bd2678a728a6c4953b0b962
michaelorina <orinamichael4@gmail.com>
2023-05-08 18:38:07 +0300

    create users

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key" : "8ef1f365-9a52-4331-84b2-187be100724c"}}
+    )
+    return res.status(r.status).json(r.data)
+  }catch(e){
+    return res.status(e.response.status).json(e.response.data)
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
